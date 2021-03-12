dee301e6d9123e963fe614349212ce2a1bc28282
Phoenix Do <777dophoenix@gmail.com>
2021-03-12 10:06:38 -0500

    Added stacks

--- amplify-infra/lib/amplify-infra-stack.ts
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -1,9 +1,23 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as amplify from "@aws-cdk/aws-amplify";
 
 export class AmplifyInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    //Creation of the Amplify Application
+    const amplifyApp = new amplify.App(this, "full-stack-dashboard", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "phoenixdo",
+        repository: "full-stack-dashboard",
+        oauthToken: cdk.SecretValue.secretsManager("full-stack-dashboard", {
+          jsonField: "full-stack-dashboard"
+        })
+      })
+    });
+
+    const masterBranch = amplifyApp.addBranch("master");
   }
 }
